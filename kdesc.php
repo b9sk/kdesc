82f52ed7fb2c0a1d35f5d86adbf3c83f9d794c36
Stop asking my name <b9sk@github.com>
2015-08-05 02:01:31 +0300

    создать колонку keywords, если не существует, при активации

--- kdesc.php
+++ kdesc.php
@@ -8,16 +8,99 @@ Author: Your mom
 Author URI: http://somegitlink.com
 License: Propietary
 */
+ini_set('error_log', 'error_output.log');
 
-function kdesc_add_meta($name, $content) {
-  echo '<meta name="'.$name.'" itemprop="'.$name.'" content="'.$content.'">';
+
+
+/**
+ * kdesc_table_exist()
+ * Проверяет есть ли искомая колонка в указаной таблице
+ *
+ * @param $tbl str Имя таблицы без префиса в которой производится поиск.
+ * @param $col str Искомая колонка.
+ * @return boolean true - col is exist, NULL - col is not exist.
+ */
+function kdesc_table_exist( $tbl = 'term_taxonomy', $col = 'keywords') {
+  global $wpdb;
+  $table_name = $wpdb->prefix . $tbl;
+  //var_dump("tbl, col, table_name", $tbl, $col, $table_name);
+  //foreach ( $GLOBALS['wpdb']->get_results( "SHOW COLUMNS FROM $table_name", ARRAY_A ) as $column_name ) { // Это работает, но возвращает геморрой
+  foreach ( $wpdb->get_col( "DESC $table_name", 0 ) as $column_name ) {
+    $table_cols[] = $column_name;
+  }
+  //var_dump($table_cols);
+  if ( array_search( $col, $table_cols ) !== FALSE )
+    return true;
+
+  //var_dump($wpdb->query("SHOW COLUMNS FROM $tbl"));
+
 }
 
-add_action( 'wp_head_add_meta', 'kdesc_add_meta', 10, 2);
+/**
+ * Установка плагина
+ */
+
+register_activation_hook( __FILE__, 'kdesc_activate' );
+
+function kdesc_activate() {
+
+  /*add_action( 'admin_notices', function() {
+    echo "<div id=\"message\" class=\"updated\"><pre>";
+    var_dump($kdesc_table_cols);
+    echo "</pre>";
+  } );*/
+
+  /*is_admin() && add_filter( 'gettext',
+
+
+    function( $translated_text, $untranslated_text, $domain )
+    {
+        $old = array(
+            "Plugin <strong>activated</strong>.",
+            "Selected plugins <strong>activated</strong>."
+        );
+
+        ob_start();
+        var_dump($kdesc_table_cols);
+        $result = ob_get_clean();
+
+        $new = "<pre>" . $kdesc_table_cols . "</pre>";
+
+        if ( in_array( $untranslated_text, $old, true ) )
+            $translated_text = $new;
+
+        return $translated_text;
+     }
+  , 99, 3 );*/
+
+  //if (!array_search( 'keywords', $kdesc_table_cols )) {
+  if ( !kdesc_table_exist('term_taxonomy', 'keywords') ) {
+    //var_dump("Time to add col to table!");
+    global $wpdb;
+    $wpdb->query("
+      ALTER TABLE wp_term_taxonomy ADD keywords LONGTEXT CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci NOT NULL AFTER description
+    ");
+
+  }
+
+}
+
+
+/**
+ * Вывод keywords и description
+ */
+
+function kdesc_add_meta( $name, $content ) {
+  if ( $content )
+    echo '<meta name="'.$name.'" itemprop="'.$name.'" content="'.$content.'">';
+}
+
+add_action( 'wp_head_add_meta', 'kdesc_add_meta', 10, 2 );
 
 add_action( 'wp_head', function() {
 
-  if (is_tag()) {
+  if ( is_tag() ) {
 
     global $wpdb;
     $kdesc_tax_id = get_query_var('tag_id');
@@ -28,7 +111,7 @@ add_action( 'wp_head', function() {
     ");
   }
 
-  if (is_category()) {
+  if ( is_category() ) {
 
     global $wpdb;
     $kdesc_tax_id = get_query_var('cat');
@@ -39,13 +122,18 @@ add_action( 'wp_head', function() {
     ");
   }
 
-  if (is_category() || is_tag()) {
+  if ( is_category() || is_tag() ) {
     do_action('wp_head_add_meta', 'keywords', $kdesc_get_data->keywords);
     do_action('wp_head_add_meta', 'description', $kdesc_get_data->description);
   }
 } );
 
+//add_action( 'admin_notices', 'kdesc_table_exist' );
+
 /*
  * Добавить поле
  * ALTER TABLE  `wp_term_taxonomy` ADD  `keywords` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL AFTER  `description`
+ *
+ * Удалить поле
+ ALTER TABLE `wp_term_taxonomy` DROP `keywords`
  */
